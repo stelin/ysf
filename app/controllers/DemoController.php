0be5fe1561a772f87a311025dbeadb64146ab8c7
lilin <794774870@qq.com>
2017-03-13 18:31:02 +0800

    cor_redis_methods

--- app/controllers/DemoController.php
+++ app/controllers/DemoController.php
@@ -18,7 +18,9 @@ class DemoController extends Controller
     {
         $redis = RedisPool::getInstance()->getConnect();
         $redis->set("stelin", 'stelin_cor');
+        $redis->mset(array('stelin2' => "stelin_cor2"));
         $redisGet = $redis->get("stelin");
+        $redisMget = $redis->mget(array('stelin',"stelin2","stelin3","stelin5","stelin6"));
         $this->outputJson(null, "this default action name=".$this->getGet('name').json_encode(Ysf::app()->params)."-".$redisGet);
     }
     public function actionShowHtml()
--- vendor/ysf/db/RedisConnect.php
+++ vendor/ysf/db/RedisConnect.php
@@ -1,7 +1,74 @@
 <?php
 namespace ysf\db;
 
+use ysf\Ysf;
+
 class RedisConnect extends \Swoole\Coroutine\Redis
 {
+    public $profilePrefix   = 'redis.';
+    public $countingPrefix  = 'redis.hit/req.';
     
+    public function set($key, $value, $expire = 0)
+    {
+        Ysf::profileStart($this->profilePrefix.__FUNCTION__);
+        if($expire > 0){
+            $ret = parent::setex($key, $expire, $value);
+        }else{
+            $ret = parent::set($key, $value);
+        }
+        Ysf::profileEnd($this->profilePrefix.__FUNCTION__);
+        
+        return $ret;
+    }
+    
+    public function get($key)
+    {
+        Ysf::profileStart($this->profilePrefix.__FUNCTION__);
+        $ret = parent::get($key);
+        Ysf::counting($this->countingPrefix.__FUNCTION__, $ret == false ? 0 : 1, 1);
+        Ysf::profileEnd($this->profilePrefix.__FUNCTION__);
+    
+        return $ret;
+    }
+    
+    public function mset($aryKV, $expire = 0)
+    {
+        Ysf::profileStart($this->profilePrefix.__FUNCTION__);
+    
+        $aryKeyValues = [];
+        foreach ($aryKV as $key => $value){
+            $aryKeyValues[$key] = $value;
+        }
+    
+        $ret = parent::mset($aryKeyValues);
+        if($expire > 0){
+            foreach ($aryKeyValues as $k => $v){
+                parent::expire($key, $expire);
+            }
+        }
+        Ysf::profileEnd($this->profilePrefix.__FUNCTION__);
+    
+        return $ret;
+    }
+    
+    public function mget(array $keys)
+    {
+        Ysf::profileStart($this->profilePrefix.__FUNCTION__);
+        $ret = parent::mget($keys);
+    
+        $data = [];
+        foreach ($ret as $index => $value){
+            if(!isset($keys[$index])){
+                continue;
+            }
+            $mkey = $keys[$index];
+            if($value != false){
+                $data[$mkey] = $value;
+            }
+        }
+        Ysf::counting($this->countingPrefix.__FUNCTION__, count($data), count($keys));
+        Ysf::profileEnd($this->profilePrefix.__FUNCTION__);
+    
+        return $data;
+    }
 }
